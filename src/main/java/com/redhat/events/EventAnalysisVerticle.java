1acfc7d44b91058e59a36d3a4e8a2295012f2c88
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-04-29 21:38:23 -0400

    Update EventAnalysisVerticle.java

--- src/main/java/com/redhat/events/EventAnalysisVerticle.java
+++ src/main/java/com/redhat/events/EventAnalysisVerticle.java
@@ -53,10 +53,10 @@ public class EventAnalysisVerticle extends AbstractVerticle {
         KafkaConsumer<String, String> customerEvents = getKafkaConsumerCustomerEvents(config,vertx);
 
         customerEvents.handler(record -> {
-            System.out.println(record.value());
+           
             Map<String, Object> value = new Gson().fromJson(record.value(),Map.class);
 
-
+ System.out.println(value);
 
                     event = new Gson().fromJson(new Gson().toJson(value), com.Event.class);
 
